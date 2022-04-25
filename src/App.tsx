732cadb9bb005b7a7fe62e40242bb4761e42ee53
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-04-25 11:00:55 +0200

    :sparkles: added Item component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import {ItemComponent} from "./components/Item";
+import {Item} from "./components/Item";
 
 function App() {
   return (
     <>
-      <ItemComponent
+      <Item
         imageUrl='https://upload.wikimedia.org/wikipedia/commons/a/a0/Andrzej_Person_Kancelaria_Senatu.jpg'
         text='Rodrigo'
       />
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,6 +1,6 @@
 import {Avatar} from "@mui/material";
 
-export const ItemComponent = (props: ItemComponentProps) => {
+export const Item = (props: ItemComponentProps) => {
   return (
     <>
       <Avatar src={props.imageUrl} sx={{width: 64, height: 64}}/>
