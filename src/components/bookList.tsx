b4f33c5a8d8b6b2032511d06a2dd23c1a0c1bf08
mategra599 <mategra599@student.polsl.pl>
2022-04-29 19:29:15 +0200

    added book list

---
+++ src/components/bookList.tsx
@@ -0,0 +1,17 @@
+import BookInList from "./bookInList";
+import { userPageMock } from "../mocks/user";
+import "././styles/bookList.css";
+import React from "react";
+
+export default function BookList() {
+    let toReturn = []
+    for(let i=0; i<userPageMock.purchaseHistory.purchases.length; i++)
+    {
+        toReturn .push ( <BookInList book={userPageMock.purchaseHistory.purchases[i].book} date={userPageMock.purchaseHistory.purchases[i].date}/>)
+    }
+    return (
+    <div className="bookList">
+        {toReturn}
+    </div>
+    )
+}
