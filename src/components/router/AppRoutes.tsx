6a8a510b71aff8312c5d1c41a7d107c5e27cf3f4
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-05-10 20:45:57 +0200

    :sparkles: added all routes

--- src/components/router/AppRoutes.tsx
+++ src/components/router/AppRoutes.tsx
@@ -3,14 +3,20 @@ import {Home} from "../../views/Home";
 import {SignIn} from "../../views/SignIn";
 import {SignUp} from "../../views/SignUp";
 import User from "../../views/User";
+import {Author} from "../../views/Author";
+import {Genre} from "../../views/Genre";
+import Settings from "../../views/Settings";
 
 export const AppRoutes = () => {
     return (
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="sign-in" element={<SignIn />} />
-          <Route path="sign-up" element={<SignUp />} />
-          <Route path="profile" element={<User />} />
-      </Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="sign-in" element={<SignIn />} />
+        <Route path="sign-up" element={<SignUp />} />
+        <Route path="authors/:id" element={<Author />} />
+        <Route path="genre/:id"  element={<Genre />} />
+        <Route path="profile" element={<User />} />
+        <Route path="profile/settings" element={<Settings />} />
+    </Routes>
     )
 }
