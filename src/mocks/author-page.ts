9a9af508f31803c682dae2b9c91fa8dde810f4bf
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-05-18 22:48:17 +0200

    :art: improved CSS section component

--- src/mocks/author-page.ts
+++ src/mocks/author-page.ts
@@ -1,83 +1,65 @@
-export const authorPageMock = {}
-// import {PageProps} from "../components/generic/page/Page";
-//
-// export const authorPageMock: PageProps = {
-//   title: "Author: JJR Tolkien",
-//   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ' +
-//     'dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea ' +
-//     'commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ' +
-//     'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut ' +
-//     'perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa ' +
-//     'quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia ' +
-//     'voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. ' +
-//     'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi ' +
-//     'tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ' +
-//     'ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea ' +
-//     'voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
-//   sections: [
-//     {
-//       title: 'Best Seller from JJR Tolkien',
-//       items: [
-//         {
-//           imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Andrzej_Person_Kancelaria_Senatu.jpg',
-//           imageSize: 120,
-//           text1: 'Rodrigo Morales',
-//           text2: 'Lanie Wody Kind'
-//         },
-//         {
-//           imageUrl: 'https://this-person-does-not-exist.com/img/avatar-ed0a92eacc2357048221df678787e30b.jpg',
-//           imageSize: 120,
-//           text1: 'Roderyk Morales',
-//           text2: 'Lanie Wody Kind'
-//         },
-//         {
-//           imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Andrzej_Person_Kancelaria_Senatu.jpg',
-//           imageSize: 120,
-//           text1: 'Rodrigo Morales',
-//           text2: 'Lanie Wody Kind'
-//         },
-//         {
-//           imageUrl: 'https://this-person-does-not-exist.com/img/avatar-1057db26a1330a28f910707446dd7264.jpg',
-//           imageSize: 120,
-//           text1: 'Andres Ortiz',
-//           text2: 'Lanie Wody Kind'
-//         },
-//         {
-//           imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Andrzej_Person_Kancelaria_Senatu.jpg',
-//           imageSize: 120,
-//           text1: 'Alice D',
-//           text2: 'Lanie Wody Queen'
-//         },
-//         {
-//           imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Andrzej_Person_Kancelaria_Senatu.jpg',
-//           imageSize: 120,
-//           text1: 'Alicia D',
-//           text2: 'Lanie Wody Queen'
-//         },
-//         {
-//           imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Andrzej_Person_Kancelaria_Senatu.jpg',
-//           imageSize: 120,
-//           text1: 'Alicja',
-//           text2: 'Lanie Wody Queen'
-//         },
-//       ]
-//     },
-//     {
-//       title: 'Books from Authors Similar to Robert Latowiec',
-//       items: [
-//         {
-//           imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Andrzej_Person_Kancelaria_Senatu.jpg',
-//           text: 'Monika'
-//         },
-//         {
-//           imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Andrzej_Person_Kancelaria_Senatu.jpg',
-//           text: 'Aleksandra'
-//         },
-//         {
-//           imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Andrzej_Person_Kancelaria_Senatu.jpg',
-//           text: 'Magdalena'
-//         }
-//       ]
-//     },
-//   ]
-// }
+import {PageProps} from "../components/generic/page/Page";
+
+export const authorPageMock: PageProps = {
+  title: "Author: JJR Tolkien",
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ' +
+    'dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea ' +
+    'commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ' +
+    'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut ' +
+    'perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa ' +
+    'quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia ' +
+    'voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. ' +
+    'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi ' +
+    'tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ' +
+    'ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea ' +
+    'voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
+  sections: [
+    {
+      title: 'Best Sellers from JJR Tolkien',
+      items: [
+        {
+          imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41FvFVUZLjL._SX328_BO1,204,203,200_.jpg',
+          imageSize: 160,
+          text1: 'The Hobbit',
+          text2: 'Lanie Wody description'
+        },
+        {
+          imageUrl: 'https://this-person-does-not-exist.com/img/avatar-ed0a92eacc2357048221df678787e30b.jpg',
+          imageSize: 160,
+          text1: 'Roderyk Morales',
+          text2: 'Lanie Wody Kind'
+        },
+        {
+          imageUrl: 'https://cdn.discordapp.com/attachments/948362388166561822/976566811267067975/16529020659115856730126820322554.png',
+          imageSize: 160,
+          text1: 'Rodrigo Morales',
+          text2: 'Lanie Wody Kind'
+        },
+        {
+          imageUrl: 'https://this-person-does-not-exist.com/img/avatar-1057db26a1330a28f910707446dd7264.jpg',
+          imageSize: 160,
+          text1: 'Andres Ortiz',
+          text2: 'Lanie Wody Kind'
+        },
+        {
+          imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Andrzej_Person_Kancelaria_Senatu.jpg',
+          imageSize: 160,
+          text1: 'Alice D',
+          text2: 'Lanie Wody Queen'
+        },
+        {
+          imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Andrzej_Person_Kancelaria_Senatu.jpg',
+          imageSize: 160,
+          text1: 'Alicia D',
+          text2: 'Lanie Wody Queen'
+        },
+        {
+          imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Andrzej_Person_Kancelaria_Senatu.jpg',
+          imageSize: 160,
+          text1: 'Alicja',
+          text2: 'Lanie Wody Queen'
+        },
+      ]
+    },
+  ]
+}
